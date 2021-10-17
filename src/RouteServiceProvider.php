eb309f3a8fbdc94e6baab922cf9025fad24780fb
HosseinAlikhani <poshtiban.esteam@gmail.com>
2021-10-17 16:22:35 +0330

    add: add service provider + register new mig directory

---
+++ src/RouteServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+namespace D3cr33\Routes;
+
+use Illuminate\Support\ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        $this->registerAndPublishMigrations();
+    }
+
+
+    private function registerAndPublishMigrations()
+    {
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->publishes([
+            __DIR__.'./../database/migrations'  =>  database_path('migrations'),
+        ],'d3cr33-route-migrations');
+    }
+}
