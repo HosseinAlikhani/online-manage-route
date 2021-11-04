d56a63f6c7ca1206dd8414ca1587c8225b8d1eac
HosseinAlikhani <poshtiban.esteam@gmail.com>
2021-11-05 00:13:06 +0330

    update: update routes table

--- database/migrations/2021_10_17_112048_create_routes_table.php
+++ database/migrations/2021_10_17_112048_create_routes_table.php
@@ -15,15 +15,17 @@ class CreateRoutesTable extends Migration
     {
         Schema::create('routes', function (Blueprint $table) {
             $table->id();
-            $table->string('request_method');
-            $table->string('name');
-            $table->string('prefix')->nullable();
-            $table->string('namespace')->nullable();
+            $table->string('request_method')->nullable();
+            $table->string('name')->nullable();
             $table->string('controller')->nullable();
             $table->string('controller_method')->nullable();
+            $table->string('prefix')->nullable();
+            $table->string('namespace')->nullable();
             $table->string('middleware')->nullable();
             $table->string('throttle')->nullable();
-            $table->string('order')->default(0);
+            $table->integer('order')->default(0);
+            $table->boolean('is_group')->default(false)->comment('determine record is group or not');
+            $table->integer('group_parent')->default(0)->comment('save group id record');
             $table->timestamps();
             $table->softDeletes();
         });
