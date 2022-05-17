e2a576d22d423f062bb1e7d29278297b2f8adfe3
hossein.alikhani <hossein.alikhani1374@gmail.com>
2022-05-17 21:50:46 +0430

    update: use constant in routes migration + declare in Route Model

--- database/migrations/2021_10_17_112048_create_routes_table.php
+++ database/migrations/2021_10_17_112048_create_routes_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use D3cr33\Routes\Contracts\Route;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,19 +14,19 @@ class CreateRoutesTable extends Migration
      */
     public function up()
     {
-        Schema::create('routes', function (Blueprint $table) {
+        Schema::create(Route::TABLE, function (Blueprint $table) {
             $table->id();
-            $table->string('request_method')->nullable();
-            $table->string('name')->nullable();
-            $table->string('controller')->nullable();
-            $table->string('controller_method')->nullable();
-            $table->string('prefix')->nullable();
-            $table->string('namespace')->nullable();
-            $table->string('middleware')->nullable();
-            $table->string('throttle')->nullable();
-            $table->integer('order')->default(0);
-            $table->boolean('is_group')->default(false)->comment('determine record is group or not');
-            $table->integer('group_parent')->default(0)->comment('save group id record');
+            $table->string(Route::REQUEST_METHOD)->nullable();
+            $table->string(Route::NAME)->nullable();
+            $table->string(Route::CONTROLLER)->nullable();
+            $table->string(Route::CONTROLLER_METHOD)->nullable();
+            $table->string(Route::PREFIX)->nullable();
+            $table->string(Route::NAMESPACE)->nullable();
+            $table->string(Route::MIDDLEWARE)->nullable();
+            $table->string(Route::MIDDLEWARE)->nullable();
+            $table->integer(Route::ORDER)->default(0);
+            $table->boolean(Route::IS_GROUP)->default(false)->comment('determine record is group or not');
+            $table->integer(Route::GROUP_PARENT)->default(0)->comment('save group id record');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -38,6 +39,6 @@ class CreateRoutesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('routes');
+        Schema::dropIfExists(Route::TABLE);
     }
 }
--- src/Contracts/Route.php
+++ src/Contracts/Route.php
@@ -3,5 +3,16 @@ namespace D3cr33\Routes\Contracts;
 
 interface Route
 {
-    
+    public const TABLE = 'routes';
+    public const REQUEST_METHOD = 'request_method';
+    public const NAME = 'name';
+    public const CONTROLLER = 'controller';
+    public const CONTROLLER_METHOD = 'controller_method';
+    public const PREFIX = 'prefix';
+    public const NAMESPACE = 'namespace';
+    public const MIDDLEWARE = 'middleware';
+    public const THROTTLE = 'throttle';
+    public const ORDER = 'order';
+    public const IS_GROUP = 'is_group';
+    public const GROUP_PARENT = 'group_parent';
 }
