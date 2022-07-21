105070748b4599afdbe36bc29803ffadc0b4af85
hossein.alikhani <hossein.alikhani1374@gmail.com>
2022-07-21 22:26:58 +0430

    update: bind RouteServiceInterface / RouteRepositoryInterface with specific service  - update comment

--- src/RouteServiceProvider.php
+++ src/RouteServiceProvider.php
@@ -1,18 +1,24 @@
 <?php
 namespace D3cr33\Routes;
 
+use D3cr33\Routes\Contracts\RouteRepositoryinterface;
+use D3cr33\Routes\Contracts\RouteServiceInterface;
+use D3cr33\Routes\Repositories\RouteRepository;
+use D3cr33\Routes\Services\RouteService;
 use Illuminate\Support\ServiceProvider;
 
 class RouteServiceProvider extends ServiceProvider
 {
     public function register()
     {
-
+        $this->app->bind(RouteServiceInterface::class, RouteService::class);
+        $this->app->bind(RouteRepositoryinterface::class, RouteRepository::class);
     }
 
     public function boot()
     {
         $this->registerAndPublishMigrations();
+
         RouteRegister::registerFromDb();
     }
 
@@ -22,7 +28,10 @@ class RouteServiceProvider extends ServiceProvider
      */
     private function registerAndPublishMigrations()
     {
+        // load migration files
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+        // publish migration files
         $this->publishes([
             __DIR__.'./../database/migrations'  =>  database_path('migrations'),
         ],'d3cr33-route-migrations');
