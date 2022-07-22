ad3d2a71ee26539f5f0d6460822372aa6373ac40
hossein.alikhani <hossein.alikhani1374@gmail.com>
2022-07-22 15:58:09 +0430

    add: add Routes + RoutesInterface contract

---
+++ src/Contracts/RoutesInterface.php
@@ -0,0 +1,8 @@
+<?php
+namespace D3cr33\Routes\Contracts;
+
+
+interface RoutesInterface
+{
+
+}
---
+++ src/Routes.php
@@ -0,0 +1,9 @@
+<?php
+namespace D3cr33\Routes;
+
+use D3cr33\Routes\Contracts\RoutesInterface;
+
+final class Routes implements RoutesInterface
+{
+
+}
